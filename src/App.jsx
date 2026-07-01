5a7338fd739ba2a58fe74b8c62a5ff3d8385e58e
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-07-01 17:12:50 +0430

    Changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useTheme } from "next-themes";
 import React from "react";
-import i18n from "./i18n/i18n.js";
+import { useTranslation } from "react-i18next";
 export default function App() {
   const { theme, setTheme } = useTheme();
+  const { t, i18n } = useTranslation();
   return (
     <div className="w-full h-full bg-gray-50 dark:bg-gray-900">
       <nav className="w-full py-3 mx-auto px-3 flex border-b dark:border-b-red-500 border-b-green-700 justify-between items-center backdrop-blur-md">
@@ -26,15 +27,36 @@ export default function App() {
           >
             System
           </button>
+          <button
+            className="py-2 px-5 bg-green-700 dark:bg-red-600 text-white rounded-md"
+            onClick={() => i18n.changeLanguage("fa")}
+          >
+            فارسی
+          </button>
+          <button
+            className="py-2 px-5 bg-green-700 dark:bg-red-600 text-white rounded-md"
+            onClick={() => i18n.changeLanguage("pa")}
+          >
+            پشتو
+          </button>
+          <button
+            className="py-2 px-5 bg-green-700 dark:bg-red-600 text-white rounded-md"
+            onClick={() => i18n.changeLanguage("en")}
+          >
+            English
+          </button>
+          <button
+            className="py-2 px-5 bg-green-700 dark:bg-red-600 text-white rounded-md"
+            onClick={() => i18n.changeLanguage("fr")}
+          >
+            French
+          </button>
         </div>
       </nav>
       <div className="w-full flex  h-screen justify-center items-center">
         <div className="w-1/2 p-8 border border-green-600 dark:border-red-500 rounded-md">
-          <h1>Hello there, Good morning</h1>
-          <p>
-            Today is a beautiful day, We are going to learn about something
-            amazing in react.js
-          </p>
+          <h1>{t("home.hero.title")}</h1>
+          <p>{t("home.hero.description")}</p>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "next-themes";
+import i18n from "./i18n/i18n.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
